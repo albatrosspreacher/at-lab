3a110d606c46159c8ea82eb5805a46b61dac45be
Nandini Proothi <46650119+nandiniproothi@users.noreply.github.com>
2021-03-04 20:36:18 +0530

    🚨

--- Lab-1/l1q1/app/src/main/java/com/example/l1q1/MainActivity.java
+++ Lab-1/l1q1/app/src/main/java/com/example/l1q1/MainActivity.java
@@ -3,11 +3,6 @@ package com.example.l1q1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Gravity;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,26 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        EditText t = (EditText) findViewById(R.id.et_Username);
-        t.setGravity(Gravity.CENTER);
-        EditText t1 = (EditText) findViewById(R.id.et_Password);
-        t1.setGravity(Gravity.CENTER);
-        EditText t2 = (EditText) findViewById(R.id.et_mobileNum);
-        t2.setGravity(Gravity.CENTER);
-        Button mButton = (Button) findViewById(R.id.bt_SignIn);
-        mButton.setOnClickListener(
-                new View.OnClickListener(){
-                    @Override
-                    public void onClick(View v) {
-                     Log.v("EditText", t.getText().toString());
-                     String regex = "^[\\w-_\\.+]*[\\w-_\\.]\\@([\\w]+\\.)+[\\w]+[\\w]$";
-                     Log.v("emailValidation", Boolean.toString((t.getText().toString().matches(regex))));
-
-                    }
-                }
-        );
     }
-
-
 }
-
