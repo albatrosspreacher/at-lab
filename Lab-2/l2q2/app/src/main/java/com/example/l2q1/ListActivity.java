3cf75b9db4480c5161f4871b55a0d54af85e7765
Nandini Proothi <46650119+nandiniproothi@users.noreply.github.com>
2021-03-05 09:22:09 +0530

    🥳

---
+++ Lab-2/l2q2/app/src/main/java/com/example/l2q1/ListActivity.java
@@ -0,0 +1,24 @@
+ package com.example.l2q1;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class ListActivity extends Activity {
+    String[] mobileArray = {"Android","IPhone","WindowsMobile","Blackberry",
+            "WebOS","Ubuntu","Windows7","Max OS X"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ArrayAdapter adapter = new ArrayAdapter<String>(this,
+                R.layout.activity_listview, mobileArray);
+
+        ListView listView = (ListView) findViewById(R.id.mobile_list);
+        listView.setAdapter(adapter);
+        
+    }
+}
---
+++ Lab-2/l2q2/app/src/main/java/com/example/l2q1/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.l2q1;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+    String[] mobileArray = {"Eggs","Bread","Vegetables","Juices",
+            "Cake"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ArrayAdapter adapter = new ArrayAdapter<String>(this,
+                R.layout.activity_listview, mobileArray);
+
+        ListView listView = (ListView) findViewById(R.id.mobile_list);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String selectedItem = (String) parent.getItemAtPosition(position);
+                Toast.makeText(getApplicationContext(),selectedItem, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
